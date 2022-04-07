c2caed7ec3b48555a64a0cb615a2b0ab1b08d2a9
osykris <osy.krisdayanti27@gmail.com>
2022-04-07 20:29:32 +0700

    update

--- public/assets/js/page/modules-datatables.js
+++ public/assets/js/page/modules-datatables.js
@@ -28,13 +28,6 @@ $("[data-checkboxes]").each(function() {
   });
 });
 
-$("#table-1").dataTable({
-  "columnDefs": [
-    { "sortable": false, "targets": [2,3] }
-  ]
-});
-$("#table-2").dataTable({
-  "columnDefs": [
-    { "sortable": false, "targets": [0,2,3] }
-  ]
-});
+$("#table-1").dataTable();
+$("#setor-transaksi").dataTable();
+
